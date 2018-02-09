7964e10c200dffd12d9fd3b673e9bb6c9790f6f8
newtonoliveira70 <newtonoliveira@bol.com..br>
2018-02-09 09:20:56 -0300

    Inicio do Projeto

---
+++ Controle_Venda/Controle_Venda/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Controle_Venda.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Controle_Venda/Controle_Venda/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Controle_Venda.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
